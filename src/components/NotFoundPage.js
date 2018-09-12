a569bc9c860fa125df7fd270a493b05a673b35a3
Dynatos <00jjqaz@gmail.com>
2018-09-12 10:11:43 -0700

    initial commit

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
-import NavBar from './NavBar/NavBar';
 
 const NotFoundPage = () => {
   return (
     <div>
-      <NavBar/>
       <h4>
         404 Page Not Found
       </h4>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import todos from './todos';
-import visibilityFilter from './visibilityFilter';
-import toggleStat from './toggleStat';
-import calculatorReducer from './calculateFix';
-import PathOfExileReducer from './PathOfExileReducer';
 
 const rootReducer = combineReducers({
-  routing: routerReducer,
-  todos,
-  visibilityFilter,
-  toggleStat,
-  calculatorReducer,
-  PathOfExileReducer
+  routing: routerReducer
 });
 
 export default rootReducer;
